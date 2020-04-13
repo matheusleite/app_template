7c58a223e7120a5f0c43c635ed15f06ceaae93e0
Matheus Leite de Menezes <matheus.menezes@cabal.com.br>
2020-04-13 20:41:14 -0300

    Facebook login integration

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_template/component/edit_text.dart';
 import 'package:app_template/util/navigation/nav_slide_from_right.dart';
 import 'package:app_template/view/login/forgot_password.dart';
@@ -6,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:getflutter/components/button/gf_button.dart';
 import 'package:getflutter/components/button/gf_social_button.dart';
 import 'package:getflutter/types/gf_button_type.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:dio/dio.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -13,6 +17,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool isFbLoggedIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,8 +82,39 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.push(context, NavSlideFromRight(page: SignUpPage()));
   }
 
-  void facebookLogin() {
+  void facebookLogin() async {
+    var facebookLogin = FacebookLogin();
+    facebookLogin.loginBehavior = FacebookLoginBehavior.nativeWithFallback;
+    var facebookLoginResult = await facebookLogin.logIn(['email']);
 
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onFbLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onFbLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+
+        var graphResponse = await Dio().get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${facebookLoginResult
+                .accessToken.token}');
+
+        var profile = json.decode(graphResponse.data);
+        print(profile.toString());
+
+        onFbLoginStatusChanged(true);
+        break;
+    }
+  }
+
+  void onFbLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
+      this.isFbLoggedIn = isLoggedIn;
+    });
   }
 
   void login() {
